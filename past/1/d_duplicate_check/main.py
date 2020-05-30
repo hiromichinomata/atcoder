9d95a5d01eb5931882143ebbab4bfd1520a1f29f
Hiromichi NOMATA <git@hiromichinomata.com>
2020-05-30 17:30:11 +0900

    [fix] past 1d

--- past/1/d_duplicate_check/main.py
+++ past/1/d_duplicate_check/main.py
@@ -4,24 +4,21 @@ import sys
 
 n = int(input().strip())
 
-arr = []
+arr = [0] * (n+1)
 for i in range(n):
-  arr.append(int(input().strip()))
-arr = sorted(arr)
+  tmp = int(input().strip())
+  arr[tmp] += 1
 
 missing = -1
-for i in range(n):
-  if i+1 != arr[i]:
-    missing = i+1
-    break
-
-prev = -1
 duplicate = -1
-for i in range(n):
-  if prev == arr[i]:
-    duplicate = arr[i]
+for i in range(1, n+1):
+  if arr[i] == 0:
+    missing = i
+  elif arr[i] > 1:
+    duplicate = i
+
+  if missing != -1 and duplicate != -1:
     break
-  prev = arr[i]
 
 if missing == -1:
   print("Correct")
