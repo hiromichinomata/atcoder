a119c73b77624536eb97e60e3dfa5559d3da2d24
Hiromichi NOMATA <git@hiromichinomata.com>
2020-06-01 00:01:34 +0900

    [fix] abc169 b

--- abc169/b/main.py
+++ abc169/b/main.py
@@ -8,11 +8,14 @@ def main():
   a = list(map(int, input().strip().split()))
   result = 1
   LIMIT = 10**18
+  if 0 in a:
+    print(0)
+    sys.exit()
   for i in a:
     result *= i
-    if i > LIMIT:
+    if result > LIMIT:
       print(-1)
-      sys.exit( )
+      sys.exit()
   print(result)
 
 main()
