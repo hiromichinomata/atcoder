231d28d76668c263408ba8eef07e051f073bad06
Hiromichi NOMATA <git@hiromichinomata.com>
2020-06-01 12:00:38 +0900

    [fix] abc169 d

--- abc169/d_div_game/main.py
+++ abc169/d_div_game/main.py
@@ -2,25 +2,44 @@ import math
 import sys
 input = sys.stdin.readline
 
+"""nを素因数分解"""
+"""2以上の整数n => [[素因数, 指数], ...]の2次元リスト"""
+
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    if arr==[]:
+        arr.append([n, 1])
+
+    return arr
+
 def main():
   n = original_n = int(input().strip())
-  max_search = int(math.sqrt(n)) + 1
+  factored = factorization(n)
   result = 0
 
   if n == 1:
     print("0")
     sys.exit()
 
-  for i in range(2, max_search):
-    if n % i == 0:
-      result += 1
-      n = n / i
-
-    if n == 1:
-      break
-
-  if n == original_n:
-    result += 1
+  for i, count in factored:
+    diff = 1
+    while count > 0:
+      count -= diff
+      if count >= 0:
+        result += 1
+        diff += 1
 
   print(result)
 
