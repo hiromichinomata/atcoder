4c60350c55463b48f1531dde451e5ce0175b1a43
Hiromichi NOMATA <git@hiromichinomata.com>
2020-06-05 22:06:26 +0900

    [add] past 3c

---
+++ past3/c_geometric_progression/main.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+input = sys.stdin.readline
+
+def main():
+  a, r, n = list(map(int, input().strip().split()))
+  v = a
+  LIMIT = 10**9
+  for _ in range(n-1):
+    v *= r
+    if v > LIMIT:
+      print('large')
+      sys.exit()
+
+  print(v)
+
+main()
