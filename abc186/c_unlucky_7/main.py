177e69b3b6de0aeef708b79906500eb29d5c871d
Hiromichi NOMATA <git@hiromichinomata.com>
2020-12-19 21:21:04 +0900

    [add] abc186c unlucky 7

---
+++ abc186/c_unlucky_7/main.py
@@ -0,0 +1,16 @@
+# pypy3
+
+import sys
+input = sys.stdin.readline
+import math
+
+def main():
+  n = int(input().strip())
+  count = 0
+  for i in range(1, n+1):
+    if '7' not in str(i) and '7' not in oct(i):
+      count += 1
+
+  print(count)
+
+main()
