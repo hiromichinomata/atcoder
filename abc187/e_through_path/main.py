61bb2f515c03cd641df7f04f0d9097914bd3ffa1
Hiromichi NOMATA <git@hiromichinomata.com>
2021-01-02 22:28:46 +0900

    [update] e wip

--- abc187/e_through_path/main.py
+++ abc187/e_through_path/main.py
@@ -4,10 +4,14 @@ import sys
 input = sys.stdin.readline
 
 def main():
-  t = int(input().strip())
-  for i in range(t):
-    n, s, k = list(map(int, input().strip().split()))
-    k = k % n
+  n = int(input().strip())
+  a = []
+  b = []
+  for i in range(n):
+    at, bt = list(map(int, input().strip().split()))
+    a.append(at)
+    b.append(bt)
+
 
   print()
 
