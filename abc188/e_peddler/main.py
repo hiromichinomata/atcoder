1a9074978565ea688aed828c71399190b496655a
Hiromichi NOMATA <git@hiromichinomata.com>
2021-01-16 10:25:41 +0900

    [fix] abc188 peddler

--- abc188/e_peddler/main.py
+++ abc188/e_peddler/main.py
@@ -2,36 +2,31 @@
 
 import sys
 input = sys.stdin.readline
-from atcoder.dsu import DSU
 
 def main():
   n, m = list(map(int, input().strip().split()))
   a = list(map(int, input().strip().split()))
   x = []
   y = []
-  tree = DSU(n)
 
   for _ in range(m):
     xt, yt = list(map(int, input().strip().split()))
     x.append(xt-1)
     y.append(yt-1)
 
+  edges = [[] for _ in range(n)]
+
   for i in range(m):
-    tree.merge(x[i], y[i])
+    edges[x[i]].append(y[i])
 
-  results = []
-  profits = [-float('inf')] * len(tree.groups())
-  index = -1
-  for l in tree.groups():
-    index += 1
-    if len(l) <= 1:
-      continue
-    edges = sorted(l)
-    min_value = 10**9
-    for i in range(len(edges)):
-      profits[index] = max(profits[index], a[edges[i]] - min_value)
-      min_value = min(min_value, a[edges[i]])
+  dp = [-float('inf')] * n
+  result = -float('inf')
 
-  print(max(profits))
+  for i in range(n-1, -1, -1):
+    for j in edges[i]:
+      dp[i] = max(dp[i], dp[j], a[j])
+    result = max(result, dp[i]-a[i])
+
+  print(result)
 
 main()
