a539b8678e0f45e5fa2d8efa78094a6b57cd2477
Hiromichi NOMATA <git@hiromichinomata.com>
2021-01-23 22:23:02 +0900

    [fix] c

--- abc189/c_mandarin_orange/main.py
+++ abc189/c_mandarin_orange/main.py
@@ -1,22 +1,20 @@
 #!/bin/python3
+# pypy3
 
 import sys
 input = sys.stdin.readline
-# from atcoder.segtree import SegTree
-
-def operation(a, b):
-  return min(a,b)
 
 def main():
   n = list(map(int, input().strip().split()))[0]
   a = list(map(int, input().strip().split()))
 
-  segtree = SegTree(operation, float('inf'), a)
   result = 0
 
   for i in range(n):
-    for j in range(i+1, n+1):
-      result = max(result, segtree.prod(i, j) * (j-i))
+    tmp_min = float('inf')
+    for j in range(i, n):
+      tmp_min = min(tmp_min, a[j])
+      result = max(result, tmp_min* (j-i+1))
   print(result)
 
 main()
