bfaaaf3476eab678e3cb502a7d2100c7cfaa23cb
Hiromichi NOMATA <git@hiromichinomata.com>
2021-01-23 22:40:57 +0900

    [wip] abc189e

--- abc189/e_rotate_and_flip/main.py
+++ abc189/e_rotate_and_flip/main.py
@@ -4,29 +4,38 @@ import sys
 input = sys.stdin.readline
 
 def main():
-  n, m = list(map(int, input().strip().split()))
-  a = list(map(int, input().strip().split()))
+  n = list(map(int, input().strip().split()))[0]
   x = []
   y = []
-
-  for _ in range(m):
+  for _ in range(n):
     xt, yt = list(map(int, input().strip().split()))
-    x.append(xt-1)
-    y.append(yt-1)
+    x.append(xt)
+    y.append(yt)
 
-  edges = [[] for _ in range(n)]
+  board = []
+  rotate = 0
+  mirror_x = True
+  mirror_y = True
+  m = list(map(int, input().strip().split()))[0]
+  for _ in range(m):
+    i, p = list(map(int, input().strip().split()))
+    if i == 1:
+      pass
+    elif i == 2:
+      pass
+    elif i == 3:
+      pass
+    else:
+      pass
 
-  for i in range(m):
-    edges[x[i]].append(y[i])
+    board.append(rotate, mirror_x, mirror_y)
 
-  dp = [-float('inf')] * n
-  result = -float('inf')
-
-  for i in range(n-1, -1, -1):
-    for j in edges[i]:
-      dp[i] = max(dp[i], dp[j], a[j])
-    result = max(result, dp[i]-a[i])
-
-  print(result)
+  q = list(map(int, input().strip().split()))[0]
+  a = []
+  b = []
+  for _ in range(q):
+    at, bt = list(map(int, input().strip().split()))
+    a.append(at)
+    b.append(bt)
 
 main()
