94b83412b93bb6bfa6cad3d5466601696a9ce3b2
Hiromichi NOMATA <git@hiromichinomata.com>
2021-02-27 20:08:41 +0900

    [add] scaffold abc193

---
+++ abc/abc193/c/main.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import sys
+input = sys.stdin.readline
+
+def main():
+  n = list(map(int, input().strip().split()))[0]
+  a = list(map(int, input().strip().split()))
+
+  result = 0
+
+  for i in range(n):
+    tmp_min = float('inf')
+    for j in range(i, n):
+      tmp_min = min(tmp_min, a[j])
+      result = max(result, tmp_min* (j-i+1))
+  print(result)
+
+main()
