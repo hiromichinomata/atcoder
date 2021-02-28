48b9ff87dc2722f861e0c6bc1c433dce7975d968
Hiromichi NOMATA <git@hiromichinomata.com>
2021-02-28 11:43:44 +0900

    [fix] abc193c unexpressed

--- abc/abc193/c_unexpressed/main.py
+++ abc/abc193/c_unexpressed/main.py
@@ -7,12 +7,12 @@ import math
 def main():
   n = list(map(int, input().strip().split()))[0]
 
-  result = 0
-  for i in range(2, round(n**0.5+1)): 
-    if round(i ** 0.5) ** 2 == i:
-      continue
-    result += max(int(math.log(n, i)) -1, 0)
+  result = set()
+  for i in range(2, round(n**0.5+1)):
+    max_power = int(math.log(n, i))
+    for j in range(2, max_power+1):
+      result.add(i**j)
 
-  print(n-result)
+  print(n-len(result))
 
 main()
