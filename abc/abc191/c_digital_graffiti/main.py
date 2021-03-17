f59fa5fed951357b71421b751ac0955bca7e69f5
Hiromichi NOMATA <git@hiromichinomata.com>
2021-03-17 19:57:34 +0900

    [add] abc191c digital graffiti

--- abc/abc191/c_digital_graffiti/main.py
+++ abc/abc191/c_digital_graffiti/main.py
@@ -3,13 +3,30 @@
 import sys
 input = sys.stdin.readline
 
+def calc(char):
+  if char == '#':
+    return 1
+  else:
+    return 0
+
+
 def main():
   h, w = list(map(int, input().strip().split()))
   s = []
   for _ in range(h):
-    st = list(map(str, input().strip().split()))
+    st = list(map(str, input().strip().split()))[0]
     s.append(st)
 
-  print(s)
+  result = 0
+  for i in range(h-1):
+    for j in range(w-1):
+      count = 0
+      for k in range(2):
+        for l in range(2):
+          count += calc(s[i+k][j+l])
+      if count == 1 or count == 3:
+        result += 1
+
+  print(result)
 
 main()
