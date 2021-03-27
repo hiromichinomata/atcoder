c3fa4306db217de4468dd0228eba504a5118c983
Hiromichi NOMATA <git@hiromichinomata.com>
2021-03-27 21:43:57 +0900

    [add] abc197d opposite

---
+++ abc/abc197/d_opposite/main.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import sys
+input = sys.stdin.readline
+import math
+import numpy as np
+
+def main():
+  n = list(map(int, input().strip().split()))[0]
+  x0, y0 = list(map(int, input().strip().split()))
+  xo, yo = list(map(int, input().strip().split()))
+  mx = (x0 + xo)/2
+  my = (y0 + yo)/2
+  x = x0 - mx
+  y = y0 - my
+  # r = ( (x0-xo)**2 + (y0-yo)**2 ) ** 0.5 / 2
+  rad = math.pi/n
+  cos = np.cos(rad)
+  sin = np.sin(rad)
+  rx = (x * cos) -  (y * sin) + mx
+  ry = (x * sin) + (y * cos) + my
+  print(rx)
+  print(ry)
+
+main()
