920867983937350d7b9a026c64b4b2687e925a05
Hiromichi NOMATA <git@hiromichinomata.com>
2021-03-27 22:24:33 +0900

    wip e

--- abc/abc197/e_traveler/main.py
+++ abc/abc197/e_traveler/main.py
@@ -5,11 +5,32 @@ input = sys.stdin.readline
 
 def main():
   n = list(map(int, input().strip().split()))[0]
-  x = [0]*n
-  c = [0]*n
+  max_x = [0]* (n+1)
+  min_x = [float('inf')] * (n+1)
+  set_c = set()
   for i in range(n):
-    x[i], c[i] = list(map(int, input().strip().split()))
+    x, c = list(map(int, input().strip().split()))
+    max_x[c] = max(max_x[c], x)
+    min_x[c] = min(min_x[c], x)
+    set_c.add(c)
+
+  list_c = sorted(list(set_c))
+  mode = 'plus'
+  x = 0
   result = 0
+  for c in list_c[0:-1]:
+    if mode == 'plus':
+      result += abs(x - min_x[c])
+      result += max_x[c] - min_x[c]
+      mode = 'minus'
+    else:
+      result += abs(x - max_x[c])
+      result += max_x[c] - min_x[c]
+      mode = 'plus'
+  c = list_c[-1]
+  result += abs(x - max_x[c])
+  result += max_x[c]
+
   print(result)
 
 main()
