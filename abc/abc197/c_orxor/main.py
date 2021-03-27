75149ac2d8b81b9559c4e180b64ca0d9b05c44cb
Hiromichi NOMATA <git@hiromichinomata.com>
2021-03-27 21:24:57 +0900

    [add] abc197c orxor wip

--- abc/abc197/c_orxor/main.py
+++ abc/abc197/c_orxor/main.py
@@ -6,14 +6,9 @@ input = sys.stdin.readline
 def main():
   n = list(map(int, input().strip().split()))[0]
   a = list(map(int, input().strip().split()))
-
+  a = sorted(a)
   result = 0
 
   for i in range(n):
-    tmp_min = float('inf')
-    for j in range(i, n):
-      tmp_min = min(tmp_min, a[j])
-      result = max(result, tmp_min* (j-i+1))
-  print(result)
 
 main()
