2a718de26f2d3eec2605987ff51d6e092a9843a5
Hiromichi NOMATA <git@hiromichinomata.com>
2021-03-29 20:31:47 +0900

    [add] abc197c orxor

--- abc/abc197/c_orxor/main.py
+++ abc/abc197/c_orxor/main.py
@@ -1,14 +1,33 @@
 #!/bin/python3
+# pypy3
 
 import sys
 input = sys.stdin.readline
+from itertools import product
 
 def main():
   n = list(map(int, input().strip().split()))[0]
   a = list(map(int, input().strip().split()))
-  a = sorted(a)
-  result = 0
+  result = float('inf')
+  for cond in product([True, False], repeat=n):
+    arr = []
+    ored = None
+    for i in range(n):
+      if ored is None:
+        ored = a[i]
+      else:
+        if cond[i]:
+          ored |= a[i]
+        else:
+          arr.append(ored)
+          ored = a[i]
+    arr.append(ored)
 
-  for i in range(n):
+    xored = arr[0]
+    for i in range(1, len(arr)):
+      xored ^= arr[i]
+    result = min(result, xored)
+
+  print(result)
 
 main()
