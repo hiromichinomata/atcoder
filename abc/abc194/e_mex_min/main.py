d0e8074452a7e3339c8f335c2ebb5bcbd86c195c
Hiromichi NOMATA <git@hiromichinomata.com>
2021-03-30 21:21:06 +0900

    [wip] abc194e

--- abc/abc194/e_mex_min/main.py
+++ abc/abc194/e_mex_min/main.py
@@ -2,16 +2,30 @@
 
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
 
 def main():
   n, m = list(map(int, input().strip().split()))
   a = list(map(int, input().strip().split()))
-  a = set(a)
-  a = sorted(list(a))
-  for i in range(len(a)):
-    if i != a[i]:
-      print(i)
-      sys.exit()
-  print(a[-1]+1)
+
+  d = defaultdict(int)
+  for i in range(m):
+    d[a[i]] += 1
+
+  checked = set(a[0:m])
+  result = float('inf')
+
+  for i in range(0,n+2):
+    if i not in checked:
+      result = i
+      break
+
+  for i in range(m,n):
+    d[a[i]] += 1
+    d[a[i-m]] -= 1
+    if d[a[i]] <= 0:
+      result = min(result, a[i])
+
+  print(result)
 
 main()
