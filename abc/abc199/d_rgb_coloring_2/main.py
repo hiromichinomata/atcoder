f77576746bb6c0f9eef2bbac1a7e909452745e96
Hiromichi NOMATA <git@hiromichinomata.com>
2021-04-24 22:07:59 +0900

    wip d

--- abc/abc199/d_rgb_coloring_2/main.py
+++ abc/abc199/d_rgb_coloring_2/main.py
@@ -5,11 +5,18 @@ input = sys.stdin.readline
 
 def main():
   n, m = list(map(int, input().strip().split()))
-  a = [0] * n
-  b = [0] * n
+  edges = [[] for i in range(n)]
   for i in range(m):
-    a[i], b[i] = list(map(int, input().strip().split()))
+    a, b = list(map(int, input().strip().split()))
+    a -= 1
+    b -= 1
+    a, b = min(a,b), max(a,b)
+    edges[a] += [b]
 
-  print(a)
+  print(edges)
+  dp = [0]*n
+
+  result = 0
+  print(result)
 
 main()
