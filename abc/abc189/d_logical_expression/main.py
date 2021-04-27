d5a6d3443f2fcfef2c6b76cdc94f4ba4e614f442
Hiromichi NOMATA <git@hiromichinomata.com>
2021-04-27 18:17:57 +0900

    Update abc189d logical expression

--- abc/abc189/d_logical_expression/main.py
+++ abc/abc189/d_logical_expression/main.py
@@ -1,14 +1,23 @@
 #!/bin/python3
 
 import sys
-input = sys.stdin.readline
-from decimal import Decimal
+# input = sys.stdin.readline
+import math
+
+if sys.platform =='ios':
+	sys.stdin=open('input1.txt')
 
 def main():
-  n = list(map(int, input().strip().split()))[0]
-  for i in range(n):
-    s = input().strip()
+	n = list(map(int, input().strip().split()))[0]
+	s = [0] * n
+	for i in range(n):
+		s[i] = list(map(str, input().strip().split()))[0]
 
-  print(result)
+	s = list(reversed(s))
+	result = 1
+	for i in range(n):
+		if s[i] == 'OR':
+			result += 2 ** (n-i)
+	print(result)
 
 main()
