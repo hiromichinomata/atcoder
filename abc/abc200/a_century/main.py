d004a7c989bfc5ffa18c9d8e3921589cc921b742
Hiromichi NOMATA <git@hiromichinomata.com>
2021-05-09 09:30:18 +0900

    Add abc200a century

---
+++ abc/abc200/a_century/main.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+# input = sys.stdin.readline
+
+if sys.platform =='ios':
+	sys.stdin=open('input1.txt')
+
+def main():
+	n = list(map(int, input().strip().split()))[0]
+	result = (n-1)//100 +1
+	print(result)
+
+main()
